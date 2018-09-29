66e7e4ec9850e4ed86b15edd71eebd232f93033b
Alejandro <alejbarros@gmail.com>
2018-09-29 16:21:06 -0700

    refactor

--- src/components/BarTop.js
+++ src/components/BarTop.js
@@ -4,12 +4,10 @@ const BarTop = () => (
   <div class="w3-container w3-theme-d5">
     <div class="w3-row w3-padding w3-gray">
         <div class="w3-col s13">
-            <a href="#" aling="center" class="w3-button w3-block w3-gray">NEWS EVERYDAY ON THE WEB</a>
+            <a aling="center" class="w3-button w3-block w3-gray">NEWS EVERYDAY ON THE WEB</a>
         </div>        
      </div>
   </div>
 )
-
-
 
 export default BarTop
